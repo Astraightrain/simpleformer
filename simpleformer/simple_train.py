e5c95fe3e2f8c383422ffc589edb102640d12650
astraightrain <sanice1229@gmail.com>
2023-12-14 22:20:49 +0900

    skeleton

--- simpleformer/simple_train.py
+++ simpleformer/simple_train.py
@@ -1,1 +1,14 @@
-'hi'
+import lightning as L
+
+
+class TransformerTrainer(L.lightning):
+    
+    def __init__(self, x):
+        pass
+
+    def configure_optimizer(self):
+        pass
+
+    def forward(self, x):
+        pass
+
